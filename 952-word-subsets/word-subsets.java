5acb9afef3cc85376bca35c19f679d7125f07580
eren8204 <147189579+eren8204@users.noreply.github.com>
2025-01-10 09:51:40 +0530

    Time: 12 ms (92.44%) | Memory: 54.9 MB (48.84%) - LeetSync

--- 952-word-subsets/word-subsets.java
+++ 952-word-subsets/word-subsets.java
@@ -1,42 +1,30 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i = 0;i<words2.length;i++){
-
-            HashMap<Character,Integer> m = new HashMap<>();
-
-            for(int j=0;j<words2[i].length();j++){
-                char c = words2[i].charAt(j);
-                m.put(c,m.getOrDefault(c,0)+1);
-            }
-
-            for(Map.Entry<Character,Integer> e : m.entrySet()){
-                map.put(e.getKey(),Math.max(map.getOrDefault(e.getKey(),0),e.getValue()));
+        int word[] = new int[26];
+        
+        for(String s : words2){
+            int temp[] = new int[26];
+            for(char c : s.toCharArray()){
+                temp[c-'a']++;
+                word[c-'a'] = Math.max(word[c-'a'],temp[c-'a']);
             }
         }
-
         List<String> ans = new ArrayList<>();
-
-        for(int i=0;i<words1.length;i++){
-
-            HashMap<Character,Integer> hm = new HashMap<>();
-
-            for(int j=0;j<words1[i].length();j++){
-                char c = words1[i].charAt(j);
-                hm.put(c,hm.getOrDefault(c,0)+1);
+        
+        for(String s : words1){
+            int pr[] = new int[26];
+            for(char c : s.toCharArray()){
+                pr[c-'a']++;
             }
-
-            int t = 0;
-            for(Map.Entry<Character,Integer> e : map.entrySet()){
-                if(hm.containsKey(e.getKey()) && hm.get(e.getKey())>=e.getValue())
-                    t+=1;
-                else
-                    break;
-            }
-            
-            if(t==map.size())
-                ans.add(words1[i]);
+            if(isParent(pr,word))
+                ans.add(s);
         }
         return ans;
+    }
+    public boolean isParent(int[] s,int[] t){
+        for(int i=0;i<26;i++)
+            if(s[i]<t[i])
+                return false;
+        return true;
     }
 }
