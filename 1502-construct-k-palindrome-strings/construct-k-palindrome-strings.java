c56441e1ec99a739b46812e84117884520823154
eren8204 <147189579+eren8204@users.noreply.github.com>
2025-01-11 11:09:32 +0530

    Time: 3 ms (100.00%) | Memory: 45.3 MB (76.62%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -2,6 +2,8 @@ class Solution {
     public boolean canConstruct(String s, int k) {
         if(s.length()<k)
             return false;
+        if(s.length()==k)
+            return true;
         int freq[] = new int[26];
 
         for(char c : s.toCharArray())
