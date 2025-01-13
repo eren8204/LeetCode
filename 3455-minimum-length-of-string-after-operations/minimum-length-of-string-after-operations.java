054c1ab0c76c0bd9f61aa6651ed41384a539950f
eren8204 <147189579+eren8204@users.noreply.github.com>
2025-01-13 23:18:29 +0530

    Time: 84 ms (37.75%) | Memory: 51.3 MB (5.30%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -1,15 +1,16 @@
 class Solution {
     public int minimumLength(String s) {
-        int ar[] = new int[26];
+        HashMap<Character,Integer> map = new HashMap<>();
         for(char c : s.toCharArray())
-            ar[c-'a']++;
+            map.put(c,map.getOrDefault(c,0)+1);
         int ans=0;
-        for(int i=0;i<26;i++){
-            if(ar[i]<3)
-                ans+=ar[i];
-            else if(ar[i]%2==0)
+        for(Map.Entry<Character,Integer> e : map.entrySet()){
+            int a = e.getValue();
+            if(a<3)
+                ans+=a;
+            else if(a%2==0)
                 ans+=2;
-            else if(ar[i]%2!=0)
+            else if(a%2!=0)
                 ans++;
         }
         return ans;
