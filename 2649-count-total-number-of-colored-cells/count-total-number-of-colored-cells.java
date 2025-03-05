a644a5234bcfbda30f9ca3f739c34a00e62ec2ed
eren8204 <147189579+eren8204@users.noreply.github.com>
2025-03-05 22:59:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (93.03%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,10 +1,6 @@
 class Solution {
     public long coloredCells(int n) {
-        long ans = 1;
-        for(int i=1;i<=n;i++){
-            ans += (i-1)*4;
-        }
-        return ans;
+        return 1 + 4L * n * (n - 1) / 2;
     }
 }
 // 1, 5, 13, 25
