08e7068670467c03abdce132e88c83b8c2b65a3f
eren8204 <147189579+eren8204@users.noreply.github.com>
2025-07-03 11:47:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (99.39%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
@@ -1,17 +1,6 @@
 class Solution {
     public char kthCharacter(int k) {
-        StringBuilder sb = new StringBuilder("a");
-        int i = 0;
-        while(k>Math.pow(2,i))
-            i++;
-        while(i-->0){
-            String s = sb.toString();
-            for(int j=0;j<s.length();j++)
-                if(s.charAt(j)=='z')
-                    sb.append('a');
-                else
-                    sb.append((char)(s.charAt(j)+1));
-        }
-        return sb.toString().charAt(k-1);
+        String s = "abbcbccdbccdcddebccdcddecddedeefbccdcddecddedeefcddedeefdeefeffgbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhibccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhicddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhideefeffgeffgfggheffgfgghfgghghhieffgfgghfgghghhifgghghhighhihiij";
+        return s.charAt(k-1);
     }
 }
